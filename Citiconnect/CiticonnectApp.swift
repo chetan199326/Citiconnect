bfcc5d2d8f0b1e409126fc0d16a0c326a2e8c5ee
Chetan Pote <chetanp@zignuts.com>
2025-01-24 15:12:40 +0530

    Initial Commit

---
+++ Citiconnect/CiticonnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiticonnectApp.swift
+//  Citiconnect
+//
+//  Created by Apple on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CiticonnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
